642244ed12224f8b35b2c1ed27e89a8e117a2adf
jujusf13 <jfaust2004@icloud.com>
2024-03-27 09:36:11 -0400

    all discussions works with database

--- app.js
+++ app.js
@@ -17,49 +17,46 @@ app.use(express.static(__dirname + '/public')); //contains css and images
 
 // mongoose and mongo sandbox routes
 
-
-app.get('/add-discussion', (req, res) => {
-    const discussion = new Discussion({
-        title: "new discussion",
-        discussion: "lorem ipsum..."
-    });
-
-    discussion.save()
-        .then((result) => {
-            res.send(result)
-        }).catch((err) => {
-            console.log(err);
-        })
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Home'});
 })
 
 app.get('/createDiscussion', (req, res) => {
     res.render('create_discussion', { title: 'Create Discussion'});
 })
 
+// all discussions
 app.get('/viewDiscussions', (req, res) => {
-    res.render('all_discussions', { title: 'View Discussions'})
-})
-
-app.get('/', (req, res) => {
-    res.render('home', { title: 'Home'});
-})
-
-
-app.get('/createAccount', (req, res) => {
-    res.render('create_account', { title: 'Create Account'});
+    Discussion.find()
+    .then((result) => {
+        res.render('all_discussions', { title: 'View Discussions', discussions: result })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 })
 
 app.get('/createAnnouncement', (req, res) => {
     res.render('create_announcement', { title: 'Create Announcement'});
 })
 
+app.get('/singleAnnouncement', (req, res) => {
+    res.render('single_announcement', { title: 'Announcement'});
+})
+
+// all announcements
+
+//single news article
+
+//all news articles
+
 
 app.get('/login', (req, res) => {
     res.render('login', { title: 'Login'});
 })
 
-app.get('/singleAnnouncement', (req, res) => {
-    res.render('single_announcement', { title: 'Announcement'});
+app.get('/createAccount', (req, res) => {
+    res.render('create_account', { title: 'Create Account'});
 })
 
 app.get('/viewProfile', (req, res) => {
